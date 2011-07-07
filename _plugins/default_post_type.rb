552188fc6477370ceacddec11e7870cac5e83a0a
Jordy Rose <jediknil@belkadan.com>
2011-07-07 13:51:11 -0700

    Default post type only applies to pages with YAML. This is a little overestimating, because it excludes posts with empty YAML, but that's better than what we had before.

--- _plugins/default_post_type.rb
+++ _plugins/default_post_type.rb
@@ -1,9 +1,9 @@
 module Jekyll
   class Post
-    alias_method :shadowbox_dpt_initialize, :initialize
-    def initialize(site, source, dir, name)
-      shadowbox_dpt_initialize(site, source, dir, name)
-      self.data['layout'] ||= site.config['default_layout'] || 'post'
+    alias_method :shadowbox_dpt_read_yaml, :read_yaml
+    def read_yaml(base, name)
+      shadowbox_dpt_read_yaml(base, name)
+      self.data['layout'] ||= site.config['default_layout'] || 'post' unless self.data.empty?
     end
   end
 end
