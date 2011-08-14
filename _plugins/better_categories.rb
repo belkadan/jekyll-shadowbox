368118bdfe196b0053a277b321b891040ae320c4
Jordy Rose <jediknil@belkadan.com>
2011-08-14 11:54:57 -0700

    Allow for categories to be specified in YAML.

--- _plugins/better_categories.rb
+++ _plugins/better_categories.rb
@@ -1,5 +1,16 @@
 module Jekyll
   class Post
+    alias_method :shadowbox_bettercategories_initialize, :initialize
+    def initialize(site, source, dir, name)
+      shadowbox_bettercategories_initialize(site, source, dir, name)
+
+      # self.categories is never empty now, but the user might have
+      # specified categories in YAML.
+      if self.shadowbox_bettercategories_categories.empty?
+        self.categories = self.data.pluralized_array('category', 'categories')
+      end
+    end
+
     alias_method :shadowbox_bettercategories_categories, :categories
     def categories
       result = shadowbox_bettercategories_categories
