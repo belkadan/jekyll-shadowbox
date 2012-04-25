ed2a31ba2a27ab374548c6320c9bb0b1f061bb89
Jordy Rose <jediknil@belkadan.com>
2012-04-25 16:28:59 -0400

    Be smarter about overriding Post's read_yaml method.
    This was actually inhibiting the 'binary?' check for post directories, causing every post directory resource to be loaded into memory. This will still be a problem without ptools, however!

--- _plugins/default_post_type.rb
+++ _plugins/default_post_type.rb
@@ -1,9 +1,16 @@
 module Jekyll
   class Post
-    alias_method :shadowbox_dpt_read_yaml, :read_yaml
-    def read_yaml(base, name)
-      shadowbox_dpt_read_yaml(base, name)
-      self.data['layout'] ||= site.config['default_layout'] || 'post' unless self.data.empty?
+    if self.instance_methods(false).include?(:read_yaml)
+      alias_method :shadowbox_dpt_read_yaml, :read_yaml
+      def read_yaml(base, name)
+        shadowbox_dpt_read_yaml(base, name)
+        self.data['layout'] ||= site.config['default_layout'] || 'post' unless self.data.empty?
+      end
+    else
+      def read_yaml(base, name)
+        super
+        self.data['layout'] ||= site.config['default_layout'] || 'post' unless self.data.empty?
+      end
     end
   end
 end
